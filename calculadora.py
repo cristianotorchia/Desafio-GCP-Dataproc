80e3c3b04c547d902ed72c79b09541d5601cb900
Cristiano Torchia Pimentel <85698340+cristianotorchia@users.noreply.github.com>
2021-11-21 21:33:04 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+import sys
+from pyspark import SparkContext, SparkConf
+if __name__ == "__main__":
+    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
+    words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
+    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
